d9c32438c93a76ac7b08eb68496ca1d31cfe6321
Darion S <darion@gmail.com>
2022-07-13 16:32:56 -0400

    feature: adding third instance

--- main.tf
+++ main.tf
@@ -263,6 +263,18 @@ resource "aws_instance" "ipfs2" {
 
 }
 
+resource "aws_instance" "ipfs3" {
+
+  ami                         = var.ec2_ami
+  associate_public_ip_address = false
+  instance_type               = var.ec2_instance_size
+  key_name                    = var.ipfs_keypair_name
+  availability_zone           = var.ec2_az3
+  vpc_security_group_ids      = [aws_security_group.ipfs.id]
+  subnet_id                   = var.ec2_subnet3
+
+}
+
 resource "aws_ebs_volume" "ipfs1" {
   availability_zone = var.ec2_az1
   size              = var.ec2_ebs_volume_size
@@ -270,6 +282,10 @@ resource "aws_ebs_volume" "ipfs1" {
 
 resource "aws_ebs_volume" "ipfs2" {
   availability_zone = var.ec2_az2
+  size              = var.ec2_ebs_volume_size
+}
+resource "aws_ebs_volume" "ipfs3" {
+  availability_zone = var.ec2_az3
   size              = var.ec2_ebs_volume_size
 }
 
@@ -283,4 +299,10 @@ resource "aws_volume_attachment" "ipfs2" {
   device_name = "/dev/sdh"
   volume_id   = aws_ebs_volume.ipfs2.id
   instance_id = aws_instance.ipfs2.id
+}
+
+resource "aws_volume_attachment" "ipfs3" {
+  device_name = "/dev/sdh"
+  volume_id   = aws_ebs_volume.ipfs3.id
+  instance_id = aws_instance.ipfs3.id
 }
